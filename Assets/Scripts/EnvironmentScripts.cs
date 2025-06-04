fb7486e2c92c7c3caa8e063996c12a801b7ae5e7
Draginalo <tropicaltisa@gmail.com>
2025-06-03 22:28:07 -0400

    Added fire particle effect and scene lighting.

---
+++ Assets/Scripts/EnvironmentScripts.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentScripts : MonoBehaviour
+{
+    [SerializeField] private Light light;
+    [SerializeField] private float maxIntensity;
+    [SerializeField] private float curveEvaluationSpeed;
+    [SerializeField] private AnimationCurve lightIntensityCurve;
+    private float currTime = 0;
+
+    private void FixedUpdate()
+    {
+        light.range = maxIntensity * lightIntensityCurve.Evaluate(currTime * curveEvaluationSpeed);
+        currTime += Time.deltaTime;
+
+        if (currTime * curveEvaluationSpeed > 1)
+        {
+            currTime = 0;
+        }
+    }
+}
